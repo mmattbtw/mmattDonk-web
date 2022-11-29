bce296893b989663793407d1931b1a20c4f37ab7
matt <30363562+mmattbtw@users.noreply.github.com>
2022-11-28 18:16:20 -0600

    Update webfinger.ts

--- src/pages/.well-known/webfinger.ts
+++ src/pages/.well-known/webfinger.ts
@@ -3,22 +3,22 @@ import type { APIRoute } from "astro";
 export const get: APIRoute = async function get() {
   return {
     body: JSON.stringify({
-      subject: "acct:donk@mas.to",
-      aliases: ["https://mas.to/@donk", "https://mas.to/users/donk"],
+      subject: "acct:donk@fosstodon.org",
+      aliases: ["https://fosstodon.org/@donk", "https://fosstodon.org/users/donk"],
       links: [
         {
           rel: "http://webfinger.net/rel/profile-page",
           type: "text/html",
-          href: "https://mas.to/@donk",
+          href: "https://fosstodon.org/@donk",
         },
         {
           rel: "self",
           type: "application/activity+json",
-          href: "https://mas.to/users/donk",
+          href: "https://fosstodon.org/users/donk",
         },
         {
           rel: "http://ostatus.org/schema/1.0/subscribe",
-          template: "https://mas.to/authorize_interaction?uri={uri}",
+          template: "https://fosstodon.org/authorize_interaction?uri={uri}",
         },
       ],
     }),
